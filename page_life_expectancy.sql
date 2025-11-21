6916490dbbdd6071c9bc2cc085a794a3323836c9
AdrianSleigh <35459625+AdrianSleigh@users.noreply.github.com>
2025-11-21 09:15:38 +0000

    Update page_life_expectancy.sql

--- page_life_expectancy.sql
+++ page_life_expectancy.sql
@@ -1,5 +1,10 @@
-----page life
-SELECT object_name,counter_name,cntr_value as PageLifeValue
-FROM sys.dm_os_performance_counters WHERE
-object_name like '%Buffer Manager%'
-AND counter_name = 'Page life expectancy'  
+--PageLifeExpectancy
+SELECT
+    SUBSTRING(counter_name, 1, 20) AS PageLifeExpectancy,
+    cntr_value AS PageLifeValue,
+    RIGHT('0' + CAST(cntr_value / 3600 AS VARCHAR(10)), 2) + ':' +
+    RIGHT('0' + CAST((cntr_value % 3600) / 60 AS VARCHAR(10)), 2) + ':' +
+    RIGHT('0' + CAST(cntr_value % 60 AS VARCHAR(10)), 2) AS TimeHMS
+FROM sys.dm_os_performance_counters
+WHERE object_name LIKE '%Buffer Manager%'
+  AND counter_name = 'Page life expectancy';
